d0367ee8d7ac254520d4336ca792c29a313f14c5
JyothiRadithyaJalla <jyothijalla49@gmail.com>
2026-03-08 12:38:31 +0530

    Initial commit, ignored credentials

---
+++ client/src/utils/firebase.js
@@ -0,0 +1,22 @@
+
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: "exam-helper-68289.firebaseapp.com",
+  projectId: "exam-helper-68289",
+  storageBucket: "exam-helper-68289.firebasestorage.app",
+  messagingSenderId: "198091553006",
+  appId: "1:198091553006:web:cd719d4ee1ebf391139bff"
+};
+
+
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app)
+
+const provider = new GoogleAuthProvider()
+
+export {auth , provider}
